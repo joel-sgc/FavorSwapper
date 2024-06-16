f163aba01686d65da01dd33135723024e2cf9ff2
joel-sgc <joeloultook@gmail.com>
2024-06-16 17:46:55 -0400

    - Potential pull to refresh fix for iOS (Hoping and praying it doesn't mess with android

--- src/components/middleware-provider.tsx
+++ src/components/middleware-provider.tsx
@@ -3,12 +3,24 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { usePathname, useRouter } from "next/navigation";
+import PullToRefresh from "pulltorefreshjs";
 import { Session } from "next-auth";
 import { ReactNode } from "react";
 
 export const MiddlewareProvider = ({ session, children, ...props }: { session: Session | null, children: ReactNode } & ThemeProviderProps ) => {
   const pathname = usePathname();
   const router = useRouter();
+
+  const standalone = window.matchMedia("(display-mode: standalone)").matches
+
+  if (standalone) {
+    PullToRefresh.init({
+      onRefresh() {
+        router.refresh()
+      },
+    })
+  }
+
 
   // If not logged in, force redirect to login
   if (!session && pathname !== '/auth/login') {
